cc936a0e7750e7f58bc97f422b7b688fa4f61c8e
Félix Lang <felixlang1004@gmail.com>
2026-06-10 12:02:40 +0200

    com

--- taches/tache7.py
+++ taches/tache7.py
@@ -17,15 +17,15 @@ line_follower = t6.LineFollower()
 action = input("Entrez les numéros de tâche à exécuter séparés d'un espace : ").strip().split()
 for i in range(len(action)):
     if action[i] == '1':
-        t1.__name__()
+        t1.__name__
     elif action[i] == '2':
-        t2.__name__()
+        t2.__name__
     elif action[i] == '3':
-        t3.__name__()
+        t3.__name__
     elif action[i] == '4':
-        t4.__name__()
+        t4.__name__
     elif action[i] == '5':
-        t5.__name__()
+        t5.__name__
     elif action[i] == '6':
-        t6.__name__()
+        t6.__name__
 
